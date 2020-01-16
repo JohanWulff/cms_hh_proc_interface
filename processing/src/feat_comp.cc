15666311a41f1ae0673b3ad2448a73f149c60452
GilesStrong <giles.strong@outlook.com>
2020-01-16 11:55:19 +0000

    featcomp+ test

--- processing/src/feat_comp.cc
+++ processing/src/feat_comp.cc
@@ -48,31 +48,31 @@ std::map<std::string, float> FeatComp::process(const TLorentzVector& b_1,
     if (FeatComp::_feat_check("dphi_httmet_met")) feats["dphi_httmet_mpt"] = FeatComp::delta_phi(h_tt_met, met);
     if (FeatComp::_feat_check("dphi_httvis_met")) feats["dphi_httvis_mpt"] = FeatComp::delta_phi(h_tt_vis, met);
     if (FeatComp::_feat_check("dphi_hbb_met"))    feats["dphi_hbb_met"]    = FeatComp::delta_phi(h_bb, met);
-    if (FeatComp::_feat_check("dphi_hbb_sv"))     feats["dphi_hbb_sv"]     = FeatComp::delta_phi(h_bb, sv);
+    if (FeatComp::_feat_check("dphi_hbb_sv"))     feats["dphi_hbb_sv"]     = FeatComp::delta_phi(h_bb, svfit);
     if (FeatComp::_feat_check("dphi_hbb_httmet")) feats["dphi_hbb_httmet"] = FeatComp::delta_phi(h_bb, h_tt_met);
 
     // Delta eta
     if (FeatComp::_feat_check("deta_l1_l2"))      feats["deta_l1_l2"]      = FeatComp::delta_eta(l_1, l_2);
     if (FeatComp::_feat_check("deta_b1_b2"))      feats["deta_l1_l2"]      = FeatComp::delta_eta(b_1, b_2);
-    if (FeatComp::_feat_check("deta_hbb_sv"))     feats["deta_hbb_sv"]     = FeatComp::delta_eta(h_bb, sv);
+    if (FeatComp::_feat_check("deta_hbb_sv"))     feats["deta_hbb_sv"]     = FeatComp::delta_eta(h_bb, svfit);
     if (FeatComp::_feat_check("deta_hbb_httmet")) feats["deta_hbb_httmet"] = FeatComp::delta_eta(h_bb, met);
 
     // Delta R
     if (FeatComp::_feat_check("dR_l1_l2"))      feats["dR_l1_l2"]      = l_1.DeltaR(l_2);
     if (FeatComp::_feat_check("dR_b1_b2"))      feats["dR_b1_b2"]      = b_1.DeltaR(b_2);
     if (FeatComp::_feat_check("dR_hbb_httmet")) feats["dR_hbb_httmet"] = h_bb.DeltaR(h_tt_met);
-    if (FeatComp::_feat_check("dR_hbb_sv"))     feats["dR_hbb_sv"]     = h_bb.DeltaR(sv);
+    if (FeatComp::_feat_check("dR_hbb_sv"))     feats["dR_hbb_sv"]     = h_bb.DeltaR(svfit);
     if (FeatComp::_feat_check("dR_b1_b2_x_h_bb_pT"))     feats["dR_b1_b2_x_h_bb_pT"]     = b_1.DeltaR(b_2)*h_bb.Pt();
     if (FeatComp::_feat_check("dR_l1_l2_x_h_tt_met_pT")) feats["dR_l1_l2_x_h_tt_met_pT"] = l_1.DeltaR(l_2)*h_tt_met.Pt();
-    if (FeatComp::_feat_check("dR_l1_l2_x_sv_pT"))       feats["dR_l1_l2_x_sv_pT"]       = l_1.DeltaR(l_2)*sv.Pt();
+    if (FeatComp::_feat_check("dR_l1_l2_x_sv_pT"))       feats["dR_l1_l2_x_sv_pT"]       = l_1.DeltaR(l_2)*svfit.Pt();
     if (FeatComp::_feat_check("dR_b1_b2_boosted_hbb"))     feats["dR_b1_b2_boosted_hbb"]     = FeatComp::delta_eta(b_1, b_2, h_bb);
     if (FeatComp::_feat_check("dR_l1_l2_boosted_htt_met")) feats["dR_l1_l2_boosted_htt_met"] = FeatComp::delta_eta(l_1, l_2, h_tt_met);
-    if (FeatComp::_feat_check("dR_l1_l2_boosted_sv"))      feats["dR_l1_l2_boosted_sv"]      = FeatComp::delta_eta(l_1, l_2, sv);
+    if (FeatComp::_feat_check("dR_l1_l2_boosted_sv"))      feats["dR_l1_l2_boosted_sv"]      = FeatComp::delta_eta(l_1, l_2, svfit);
     
     // Masses
     if (FeatComp::_feat_check("h_tt_met_mt"))  feats["h_tt_met_mt"]  = FeatComp::calc_mt(h_tt_met, met);
     if (FeatComp::_feat_check("diH_mass_met")) feats["diH_mass_met"] = (h_bb+h_tt_met).M();
-    if (FeatComp::_feat_check("diH_mass_sv"))  feats["diH_mass_sv"]  = (h_bb+sv).M();
+    if (FeatComp::_feat_check("diH_mass_sv"))  feats["diH_mass_sv"]  = (h_bb+svfit).M();
     if (FeatComp::_feat_check("diH_mass_vis")) feats["diH_mass_vis"] = (h_bb+h_tt_vis).M();
     if (FeatComp::_feat_check("diH_mass_X"))   feats["diH_mass_X"]   = (h_bb+h_tt_met).M()-h_tt_met.M()-h_bb.M();
 
@@ -80,27 +80,27 @@ std::map<std::string, float> FeatComp::process(const TLorentzVector& b_1,
     if (FeatComp::_feat_check("phi_met")) feats["phi_met"] = FeatComp::calc_phi(l_1, l_2, b_1, b_2, h_bb+h_tt_met);
     if (FeatComp::_feat_check("phi"))     feats["phi"]     = FeatComp::calc_phi(l_1, l_2, b_1, b_2, hh);
     if (FeatComp::_feat_check("phi1_met")) feats["phi1_met"] = FeatComp::calc_phi_1(l_1, l_2, h_tt_met, h_bb+h_tt_met);
-    if (FeatComp::_feat_check("phi1"))     feats["phi1"]     = FeatComp::calc_phi_1(l_1, l_2, sv, hh);
+    if (FeatComp::_feat_check("phi1"))     feats["phi1"]     = FeatComp::calc_phi_1(l_1, l_2, svfit, hh);
     if (FeatComp::_feat_check("phi2_met")) feats["phi2_met"] = FeatComp::calc_phi_1(b_1, b_2, h_tt_met, h_bb+h_tt_met);
-    if (FeatComp::_feat_check("phi2"))     feats["phi2"]     = FeatComp::calc_phi_1(b_1, b_2, sv, hh);
+    if (FeatComp::_feat_check("phi2"))     feats["phi2"]     = FeatComp::calc_phi_1(b_1, b_2, svfit, hh);
     if (FeatComp::_feat_check("costheta_star_met")) feats["costheta_star_met"] = FeatComp::calc_cos_delta_star(h_tt_met, h_bb+h_tt_met);
-    if (FeatComp::_feat_check("costheta_star"))     feats["costheta_star"]     = FeatComp::calc_cos_delta_star(sv, hh);
+    if (FeatComp::_feat_check("costheta_star"))     feats["costheta_star"]     = FeatComp::calc_cos_delta_star(svfit, hh);
     if (FeatComp::_feat_check("costheta_l1_httmet"))      feats["costheta_l1_httmet"]      = FeatComp::calc_cos_delta(l_1, h_tt_mpt);
-    if (FeatComp::_feat_check("costheta_l1_htt"))         feats["costheta_l1_httmet"]      = FeatComp::calc_cos_delta(l_1, sv);
+    if (FeatComp::_feat_check("costheta_l1_htt"))         feats["costheta_l1_httmet"]      = FeatComp::calc_cos_delta(l_1, svfit);
     if (FeatComp::_feat_check("costheta_l2_httmet"))      feats["costheta_l2_httmet"]      = FeatComp::calc_cos_delta(l_2, h_tt_mpt);
-    if (FeatComp::_feat_check("costheta_l2_htt"))         feats["costheta_l2_httmet"]      = FeatComp::calc_cos_delta(l_2, sv);
+    if (FeatComp::_feat_check("costheta_l2_htt"))         feats["costheta_l2_httmet"]      = FeatComp::calc_cos_delta(l_2, svfit);
     if (FeatComp::_feat_check("costheta_met_httmet"))     feats["costheta_met_httmet"]     = FeatComp::calc_cos_delta(met, h_tt_mpt);
-    if (FeatComp::_feat_check("costheta_met_htt"))        feats["costheta_met_htt"]        = FeatComp::calc_cos_delta(met, sv);
+    if (FeatComp::_feat_check("costheta_met_htt"))        feats["costheta_met_htt"]        = FeatComp::calc_cos_delta(met, svfit);
     if (FeatComp::_feat_check("costheta_met_hbb"))        feats["costheta_met_hbb"]        = FeatComp::calc_cos_delta(met, h_bb);
     if (FeatComp::_feat_check("costheta_b1_hbb"))         feats["costheta_b1_hbb"]         = FeatComp::calc_cos_delta(b_1, h_bb);
     if (FeatComp::_feat_check("costheta_b2_hbb"))         feats["costheta_b2_hbb"]         = FeatComp::calc_cos_delta(b_2, h_bb);
-    if (FeatComp::_feat_check("costheta_htt_hh_vis"))     feats["costheta_htt_hh_vis"]     = FeatComp::calc_cos_delta(sv, h_tt_vis+h_bb);
+    if (FeatComp::_feat_check("costheta_htt_hh_vis"))     feats["costheta_htt_hh_vis"]     = FeatComp::calc_cos_delta(svfit, h_tt_vis+h_bb);
     if (FeatComp::_feat_check("costheta_htt_met_hh_vis")) feats["costheta_htt_met_hh_vis"] = FeatComp::calc_cos_delta(h_tt_met, h_tt_vis+h_bb);
     if (FeatComp::_feat_check("costheta_hbb_hh_vis"))     feats["costheta_hbb_hh_vis"]     = FeatComp::calc_cos_delta(h_bb, h_tt_vis+h_bb);
-    if (FeatComp::_feat_check("costheta_htt_hh_met"))     feats["costheta_htt_hh_met"]     = FeatComp::calc_cos_delta(sv, h_tt_met+h_bb);
+    if (FeatComp::_feat_check("costheta_htt_hh_met"))     feats["costheta_htt_hh_met"]     = FeatComp::calc_cos_delta(svfit, h_tt_met+h_bb);
     if (FeatComp::_feat_check("costheta_htt_mpt_hh_met")) feats["costheta_htt_mpt_hh_met"] = FeatComp::calc_cos_delta(h_tt_met, h_tt_met+h_bb);
     if (FeatComp::_feat_check("costheta_hbb_hh_met"))     feats["costheta_hbb_hh_met"]     = FeatComp::calc_cos_delta(h_bb, h_tt_met+h_bb);
-    if (FeatComp::_feat_check("costheta_htt_hh"))         feats["costheta_htt_hh_vis"]     = FeatComp::calc_cos_delta(sv, hh);
+    if (FeatComp::_feat_check("costheta_htt_hh"))         feats["costheta_htt_hh_vis"]     = FeatComp::calc_cos_delta(svfit, hh);
     if (FeatComp::_feat_check("costheta_htt_mpt_hh"))     feats["costheta_htt_mpt_hh_vis"] = FeatComp::calc_cos_delta(h_tt_met, hh);
     if (FeatComp::_feat_check("costheta_hbb_hh"))         feats["costheta_hbb_hh_vis"]     = FeatComp::calc_cos_delta(h_bb, hh);            
 
