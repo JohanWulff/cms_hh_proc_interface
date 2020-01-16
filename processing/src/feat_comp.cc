4246bdb277db8c11afed39bba3c0b0949152c367
GilesStrong <giles.strong@outlook.com>
2020-01-16 15:17:44 +0000

    args fix

--- processing/src/feat_comp.cc
+++ processing/src/feat_comp.cc
@@ -1,6 +1,6 @@
 #include "cms_hh_proc_interface/processing/interface/feat_comp.hh"
 
-FeatComp::FeatComp(bool, std::set<std::string> requested, bool use_deep_csv, bool verbose) {
+FeatComp::FeatComp(bool return_all, std::set<std::string> requested, bool use_deep_csv, bool verbose) {
     _all = return_all;
     _requested = requested;
     _use_deep_csv = use_deep_csv;
